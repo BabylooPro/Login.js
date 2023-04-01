231c719eb05ae74adaa92a7b9231da29778df790
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2023-04-01 23:05:44 +0200

    first commit

---
+++ scrollreveal.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.sr', {
+   delay: 250,
+   duration: 1000,
+   scale: 0.8,
+   easing: 'cubic-bezier(0.5, 0, 0, 1)',
+   distance: '0px',
+   origin: 'bottom',
+   reset: false
+});
